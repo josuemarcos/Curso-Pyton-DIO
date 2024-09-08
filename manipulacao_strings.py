66c982dc78ca39991e3a6426574cc174b61cc91f
josuemarcos <josue.fernandes@ee.ufcg.edu.br>
2024-09-08 14:57:32 +0000

    Início da aula sobre manipulação de strings

---
+++ manipulacao_strings.py
@@ -0,0 +1,16 @@
+nome = "jOsUÉ"
+
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+
+texto = "  Olá Mundo!   "
+
+print(texto.strip() + ".")
+print(texto.lstrip() + ".")
+print(texto.rstrip() + ".")
+
+linguagem = "Python"
+
+print(linguagem.center(10, "-"))
+print("-".join(linguagem))
